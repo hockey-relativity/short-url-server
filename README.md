82afcc4b4c2922e6f858339225179f9145942f4e
Justin Barber <Justin.Barber@genesys.com>
2016-08-05 20:28:02 -0400

    1.0

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # short-url-server
 A tiny short url generator server with an API
+
+### How To Use
+1. Set the config parameters if you want
+2. Run with `npm listen`
+
+### If You Want
+3. Fork it
+4. Write your UI in the public folder
---
+++ index.js
@@ -0,0 +1,56 @@
+'use strict';
+
+let config = require('./config.json');
+
+const DATABASE_FILE = config.db.file;
+const DATABASE_TABLE = config.db.table;
+
+const ADMIN_PAGE = config.admin.page;
+const PORT = config.server.port;
+const SHORT_URL_SUBDIR = config.shorturls.subdir;
+
+const TABLE_CREATE_STMT = `CREATE TABLE IF NOT EXISTS ${DATABASE_TABLE} (id INTEGER PRIMARY KEY, target TEXT, added INTEGER)`;
+const URL_FETCH_STMT = `SELECT target FROM ${DATABASE_TABLE} WHERE id=$id`;
+const URL_INSERT_STMT = `INSERT INTO ${DATABASE_TABLE}(target, added) VALUES($target, $added)`;
+
+let express = require('express');
+let bodyParser = require('body-parser');
+let sqlite3 = require('sqlite3');
+let urlCheckRegex = require('url-check-regex');
+
+let app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+let db = new sqlite3.Database(DATABASE_FILE);
+db.run(TABLE_CREATE_STMT);
+
+app.get(`${SHORT_URL_SUBDIR}/:id`, function(req, res) {
+  db.get(URL_FETCH_STMT, req.params.id, function(err, rows) {
+    res.redirect(rows.target);
+  })
+})
+
+app.post(ADMIN_PAGE, function(req, res) {
+  let target = req.body.target;
+  if(urlCheckRegex.test(target)) {
+    db.run(URL_INSERT_STMT, target, Date.now(), function(err) {
+      res.json({
+        id: this.lastID,
+        url: `${SHORT_URL_SUBDIR}/${this.lastID}`,
+      });
+    });
+  }
+
+  else {
+    res.json({
+      error: 'Invalid url',
+    });
+  }
+});
+// on close, close db
+
+app.listen(PORT, function () {
+  console.log(`Running on port ${PORT}!`);
+});
